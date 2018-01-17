e4af221e74aeec63e786c79740a26e18b23bd19d
Pavel Martynau <dunkel@tut.by>
2018-01-17 01:37:44 -0800

    Corrected comment.

--- TopOfStream/TopOfStream.cpp
+++ TopOfStream/TopOfStream.cpp
@@ -11,7 +11,7 @@ using namespace std;
 * functionality:
 *
 * 1. Constructor gets limit of stream size
-* 2. Method add gets key and value of types TKey and TValue respectively and performce adding item to
+* 2. Method "add" gets key and value of types TKey and TValue respectively and performs adding item to
 *    the stream
 * 3. Implemented class should always store up to <limit> number of top (biggest) items
 * 4. Class should provide functionality to traverse all its currently stored items,
